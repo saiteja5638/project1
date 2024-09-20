e8d1cd527e57cc016a22290c5fbdbf362ecfd42f
saiteja cherviralla <saiteja.ch.st1@gmail.com>
2024-09-20 07:36:36 +0000

    new changes

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,6 +7,22 @@ sap.ui.define(
   
       return BaseController.extend("project1.controller.App", {
         onInit: function() {
+        },
+        onSideNavButtonPress:function(oEvent)
+        {
+          if(oEvent.mParameters.item.mProperties.key=="Home")
+          {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(that);
+              oRouter.navTo("View1");
+          }
+          else
+          {
+            var oToolPage = this.byId("toolPage");
+            var bSideExpanded = oToolPage.getSideExpanded();
+  
+            oToolPage.setSideExpanded(!oToolPage.getSideExpanded());
+          }
+       
         }
       });
     }
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,6 +8,23 @@ function (Controller) {
         onInit: function () {
             that = this;
         },
+        onUpload:function(Event)
+        {
+            let upload_item =  that.byId("_IDGenFileUploader")   
+            let get_upload_item = Event.getParameter('files')[0]
+            
+            let fileReader =  new FileReader()
+
+            fileReader.onload = function(oEvent)
+            {
+                let Data = oEvent.target.result
+                console.log(Data)
+
+                
+            }.bind(this)
+
+            fileReader.readAsDataURL(get_upload_item)
+        },
         ReadCall:function()
         {
             that.getOwnerComponent().getModel().read("/QUOT_HEADER_DATA",{
@@ -20,6 +37,69 @@ function (Controller) {
                     console.log(error)
                 }
             })
+        },
+        onListBinding:function()
+        {
+            let getKey = that.byId("IconTabFilter").mAggregations._header.getSelectedKey()
+            if (getKey == "Plantsinfo") {
+
+                if(that.byId("ListData").getItems().length>0)
+                    {
+                        that.byId("ListData").getItems().forEach(i=>{
+                           that.byId("ListData").removeItem(i)
+                        })
+                        
+                    }
+
+                that.getOwnerComponent().getModel().read("/QUOT_HEADER_DATA",{
+                    success:function(response)
+                    {
+                        let res_data = response.results;
+                        let List =  that.byId("ListData")
+                        res_data.forEach(element => {
+                            let Item = new sap.m.StandardListItem({
+                                 title:element.ZQUOTATION,
+                                 description:element.NAME1,
+                                 info:element.ORT01
+                            })
+                            List.addItem(Item)
+                        });
+                    },
+                    error:function(error)
+                    {
+                        console.log(error)
+                    }
+                })
+
+            }
+            if (getKey == "QualityInfo") {
+                if(that.byId("ListData").getItems().length>0)
+                    {
+                        that.byId("ListData").getItems().forEach(i=>{
+                           that.byId("ListData").removeItem(i)
+                        })
+                        
+                    }
+                    that.getOwnerComponent().getModel().read("/QUOT_ITEM_DATA",{
+                        success:function(response)
+                        {
+                            let res_data = response.results;
+                            let List =  that.byId("ListData")
+                            res_data.forEach(element => {
+                                let Item = new sap.m.StandardListItem({
+                                     title:element.MATNR,
+                                     description:element.MAKTX,
+                                     info:element.ZQUOTATION
+                                })
+                                List.addItem(Item)
+                            });
+                        },
+                        error:function(error)
+                        {
+                            console.log(error)
+                        }
+                    })
+            }
         }
 
     });
